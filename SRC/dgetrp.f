752d36e67a2c7ec31b249eb20b8ae6408c42b14e
Christoph Conrads <gitlab@christoph-conrads.name>
2024-01-03 19:07:44 +0100

    xGETRP: add documentation

--- SRC/dgetrp.f
+++ SRC/dgetrp.f
@@ -1,23 +1,115 @@
-* Copyright 2021 Christoph Conrads
-      SUBROUTINE DGETRP( M, N, X, LDX, Y, LDY, INFO )
+*> \brief \b DGETRP computes the transpose a matrix.
 *
-*  -- LAPACK driver routine --
+*  =========== DOCUMENTATION ===========
+*
+* Online html documentation available at
+*            http://www.netlib.org/lapack/explore-html/
+*
+*> \htmlonly
+*> Download DGETRP + dependencies
+*> <a href="http://www.netlib.org/cgi-bin/netlibfiles.tgz?format=tgz&filename=/lapack/lapack_routine/dgetrp.f">
+*> [TGZ]</a>
+*> <a href="http://www.netlib.org/cgi-bin/netlibfiles.zip?format=zip&filename=/lapack/lapack_routine/dgetrp.f">
+*> [ZIP]</a>
+*> <a href="http://www.netlib.org/cgi-bin/netlibfiles.txt?format=txt&filename=/lapack/lapack_routine/dgetrp.f">
+*> [TXT]</a>
+*> \endhtmlonly
+*
+*  Definition:
+*  ===========
+*
+*       SUBROUTINE DGETRP( M, N, S, LDS, D, LDD, INFO )
+*
+*       .. Scalar Arguments ..
+*       INTEGER            M, N, LDD, LDS, INFO
+*       ..
+*       .. Array Arguments ..
+*       REAL               S( LDS, * ), D( LDD, * )
+*       ..
+*
+*
+*> \par Purpose:
+*  =============
+*>
+*> \verbatim
+*>
+*> DGETRP computes the transpose of the real M-by-N source matrix S and
+*> stores the result in the real N-by-M destination matrix D.
+*> \endverbatim
+*
+*  Arguments:
+*  ==========
+*
+*> \param[in] M
+*> \verbatim
+*>          M is INTEGER
+*>          The number of rows of the matrix S and the number of columns
+*>          of the matrix D.
+*> \endverbatim
+*>
+*> \param[in] N
+*> \verbatim
+*>          N is INTEGER
+*>          The number of columns of the matrix S and the number of rows
+*>          of the matrix D.
+*> \endverbatim
+*>
+*> \param[in] S
+*> \verbatim
+*>          S is REAL array, dimension ( LDS, N )
+*>          The matrix to be transposed.
+*> \endverbatim
+*>
+*> \param[in] LDS
+*> \verbatim
+*>          LDS is INTEGER
+*>          The leading dimension of S. LDS >= MAX(1,M).
+*> \endverbatim
+*>
+*> \param[out] D
+*> \verbatim
+*>          D is REAL array, dimension
+*>          On exit, D contains the transpose of S.
+*> \endverbatim
+*>
+*> \param[in] LDD
+*> \verbatim
+*>          LDD is INTEGER
+*>          The leading dimension of D. LDD >= MAX(1,N).
+*> \endverbatim
+*>
+*> \param[out] INFO
+*> \verbatim
+*>          INFO is INTEGER
+*>          = 0: successful exit
+*>          < 0: if INFO = -i, the i-th argument has an illegal value
+*> \endverbatim
+*
+*  Authors:
+*  ========
+*
+*> \author Christoph Conrads
+*
+*  =====================================================================
+      SUBROUTINE DGETRP( M, N, S, LDS, D, LDD, INFO )
+*
+*  -- LAPACK computational routine --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd. --
 *
       IMPLICIT NONE
 *     .. Scalar Arguments ..
-      INTEGER            M, N, LDX, LDY, INFO
+      INTEGER            M, N, LDS, LDD, INFO
 *     ..
 *     .. Array Arguments ..
-      DOUBLE PRECISION   X( LDX, * ), Y( LDY, * )
+      DOUBLE PRECISION   S( LDS, * ), D( LDD, * )
 *     ..
 *
 *  =====================================================================
 *
 *     .. Parameters ..
       INTEGER            B
-*     64 byte cache line size divided by eight bytes for a double
+*     Assumed 64 byte cache line size divided by eight bytes per DOUBLE
       PARAMETER          ( B = 8 )
 *     ..
 *     .. Local Scalars ..
@@ -39,11 +131,11 @@
          INFO = -1
       ELSE IF( N.LT.0 ) THEN
          INFO = -2
-      ELSE IF( LDX.LT.MAX( 1, M ) ) THEN
+      ELSE IF( LDS.LT.MAX( 1, M ) ) THEN
          INFO = -4
-      ELSE IF( LDY.LT.MAX( 1, N ) ) THEN
+      ELSE IF( LDD.LT.MAX( 1, N ) ) THEN
          INFO = -6
-      ENDIF
+      END IF
 *
       IF( INFO.NE.0 ) THEN
          CALL XERBLA( 'DGETRP', -INFO )
@@ -56,11 +148,11 @@
          DO I = 1, M + B - 1, B
             DO L = J, MIN( J + B, N )
                DO K = I, MIN( I + B, M )
-                  Y( L, K ) = X( K, L )
-               ENDDO
-            ENDDO
-         ENDDO
-      ENDDO
+                  D( L, K ) = S( K, L )
+               END DO
+            END DO
+         END DO
+      END DO
 *
       RETURN
 *
--- SRC/sgetrp.f
+++ SRC/sgetrp.f
@@ -1,23 +1,115 @@
-* Copyright 2021 Christoph Conrads
-      SUBROUTINE SGETRP( M, N, X, LDX, Y, LDY, INFO )
+*> \brief \b SGETRP computes the transpose of a matrix.
 *
-*  -- LAPACK driver routine --
+*  =========== DOCUMENTATION ===========
+*
+* Online html documentation available at
+*            http://www.netlib.org/lapack/explore-html/
+*
+*> \htmlonly
+*> Download SGETRP + dependencies
+*> <a href="http://www.netlib.org/cgi-bin/netlibfiles.tgz?format=tgz&filename=/lapack/lapack_routine/sgetrp.f">
+*> [TGZ]</a>
+*> <a href="http://www.netlib.org/cgi-bin/netlibfiles.zip?format=zip&filename=/lapack/lapack_routine/sgetrp.f">
+*> [ZIP]</a>
+*> <a href="http://www.netlib.org/cgi-bin/netlibfiles.txt?format=txt&filename=/lapack/lapack_routine/sgetrp.f">
+*> [TXT]</a>
+*> \endhtmlonly
+*
+*  Definition:
+*  ===========
+*
+*       SUBROUTINE SGETRP( M, N, S, LDS, D, LDD, INFO )
+*
+*       .. Scalar Arguments ..
+*       INTEGER            M, N, LDD, LDS, INFO
+*       ..
+*       .. Array Arguments ..
+*       REAL               S( LDS, * ), D( LDD, * )
+*       ..
+*
+*
+*> \par Purpose:
+*  =============
+*>
+*> \verbatim
+*>
+*> SGETRP computes the transpose of the real M-by-N source matrix S and
+*> stores the result in the real N-by-M destination matrix D.
+*> \endverbatim
+*
+*  Arguments:
+*  ==========
+*
+*> \param[in] M
+*> \verbatim
+*>          M is INTEGER
+*>          The number of rows of the matrix S and the number of columns
+*>          of the matrix D.
+*> \endverbatim
+*>
+*> \param[in] N
+*> \verbatim
+*>          N is INTEGER
+*>          The number of columns of the matrix S and the number of rows
+*>          of the matrix D.
+*> \endverbatim
+*>
+*> \param[in] S
+*> \verbatim
+*>          S is REAL array, dimension ( LDS, N )
+*>          The matrix to be transposed.
+*> \endverbatim
+*>
+*> \param[in] LDS
+*> \verbatim
+*>          LDS is INTEGER
+*>          The leading dimension of S. LDS >= MAX(1,M).
+*> \endverbatim
+*>
+*> \param[out] D
+*> \verbatim
+*>          D is REAL array, dimension
+*>          On exit, D contains the transpose of S.
+*> \endverbatim
+*>
+*> \param[in] LDD
+*> \verbatim
+*>          LDD is INTEGER
+*>          The leading dimension of D. LDD >= MAX(1,N).
+*> \endverbatim
+*>
+*> \param[out] INFO
+*> \verbatim
+*>          INFO is INTEGER
+*>          = 0: successful exit
+*>          < 0: if INFO = -i, the i-th argument has an illegal value
+*> \endverbatim
+*
+*  Authors:
+*  ========
+*
+*> \author Christoph Conrads
+*
+*  =====================================================================
+      SUBROUTINE SGETRP( M, N, S, LDS, D, LDD, INFO )
+*
+*  -- LAPACK computational routine --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
 *  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd. --
 *
       IMPLICIT NONE
 *     .. Scalar Arguments ..
-      INTEGER            M, N, LDX, LDY, INFO
+      INTEGER            M, N, LDS, LDD, INFO
 *     ..
 *     .. Array Arguments ..
-      REAL               X( LDX, * ), Y( LDY, * )
+      REAL               S( LDS, * ), D( LDD, * )
 *     ..
 *
 *  =====================================================================
 *
 *     .. Parameters ..
       INTEGER            B
-*     64 byte cache line size divided by four bytes for a float
+*     Assumed 64 byte cache line size divided by four bytes per REAL
       PARAMETER          ( B = 16 )
 *     ..
 *     .. Local Scalars ..
@@ -39,11 +131,11 @@
          INFO = -1
       ELSE IF( N.LT.0 ) THEN
          INFO = -2
-      ELSE IF( LDX.LT.MAX( 1, M ) ) THEN
+      ELSE IF( LDS.LT.MAX( 1, M ) ) THEN
          INFO = -4
-      ELSE IF( LDY.LT.MAX( 1, N ) ) THEN
+      ELSE IF( LDD.LT.MAX( 1, N ) ) THEN
          INFO = -6
-      ENDIF
+      END IF
 *
       IF( INFO.NE.0 ) THEN
          CALL XERBLA( 'SGETRP', -INFO )
@@ -56,11 +148,11 @@
          DO I = 1, M + B - 1, B
             DO L = J, MIN( J + B, N )
                DO K = I, MIN( I + B, M )
-                  Y( L, K ) = X( K, L )
-               ENDDO
-            ENDDO
-         ENDDO
-      ENDDO
+                  D( L, K ) = S( K, L )
+               END DO
+            END DO
+         END DO
+      END DO
 *
       RETURN
 *
